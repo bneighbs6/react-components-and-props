fd0655d3c0092fcabc3e430518c28409c020311d
Brett <brett_neighbors@yahoo.com>
2024-01-06 11:11:13 -0700

    Edited WB.js to have inline styling

--- src/WelcomeBack.js
+++ src/WelcomeBack.js
@@ -1,8 +1,22 @@
 // WelcomeBack.js
-
-
 function WelcomeBack({ name = "valued customer" }) {
-  return <p>Welcome back, {name}!</p>;
+  const pStyle = {
+    color: "black",
+    fontFamily: "sans-serif",
+  };
+  const userName = {
+    padding: "2px 4px",
+    marginRight: "2px",
+    backgroundColor: "#000",
+    color: "#fff",
+    borderRadius: "4px",
+  };
+
+  return (
+    <p style={pStyle}>
+      Welcome back, <span style={userName}>{name}</span>!
+    </p>
+  );
 }
 
 export default WelcomeBack;
